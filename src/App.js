f0e76fa139d7ae53adf77ca9cda4112f9c23bd62
pcanjura <pcanjura@gmail.com>
2020-07-13 02:51:26 -0600

    actualizacion 13072020

--- src/App.js
+++ src/App.js
@@ -1,16 +1,110 @@
 import React, {Component} from 'react';
 import Buscador from './components/Buscador';
+import Resultado from './components/Resultado';
+
 
 
 class App extends Component {
+
+  state = {
+    termino : '', 
+    imagenes : [], 
+    pagina : ''
+  }
+
+
+  scroll = () => {
+    const elemento = document.querySelector('.jumbotron');
+    elemento.scrollIntoView('smooth', 'end'); 
+  }
+
+  paginaAnterior = () => {
+      //leer el state de la pagina actual 
+      let pagina = this.state.pagina;
+
+      //Si la pagina es 1 ya no ir hacia atras
+      if (pagina === 1) return null;
+
+      //Restar uno a la pagina actual
+      pagina -= 1;
+
+      //Agregar el cambio al state
+      this.setState({
+        pagina
+      }, () => {
+        this.consultarApi();
+        this.scroll();
+      });
+
+
+      //console.log(pagina);
+
+  }
+
+  paginaSiguiente = () => {
+      //leer el state de la pagina actual 
+      let pagina = this.state.pagina;
+
+      //Sumar uno a la pagina actual
+      pagina += 1;
+
+      //Agregar el cambio al state
+      this.setState({
+        pagina
+      }, () => {
+        this.consultarApi();
+        this.scroll();
+      });
+
+
+      //console.log(pagina);
+
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino; 
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=17453024-3024a833795a3506290247119&q=${termino}&per_page=30&page=${pagina}`;
+
+    //console.log(url);
+
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(resultado => this.setState( { imagenes : resultado.hits}))
+
+  }
+
+
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino : termino,
+      pagina : 1
+    }, () => {
+      this.consultarApi();
+    })
+
+
+  }
+
+
   render(){
     return (
       <div className="app container">
-        <div className="jumbotron">
-          <p className="lead text-center">Buscador de Imágenes</p>
+          <div className="jumbotron">
+              <p className="lead text-center">Buscador de Imágenes</p>
 
-          <Buscador/>
-        </div>
+              <Buscador
+              datosBusqueda={this.datosBusqueda }            
+              />
+          </div>
+         <div className="row justify-content-center">
+                <Resultado
+                    imagenes={this.state.imagenes}
+                    paginaAnterior={this.paginaAnterior}
+                    paginaSiguiente={this.paginaSiguiente}
+                />
+         </div>
       </div>
     );
 
--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
 
 class Buscador extends Component {
+
+    
+    busquedaRef = React.createRef();
+
+    
+    obtenerDatos = (e) => {
+        e.preventDefault();
+        //Tomamos el valor del input
+        const termino = this.busquedaRef.current.value;
+        
+        //Lo enviamos al componente principal
+        this.props.datosBusqueda(termino);
+    }
+
     render(){
         return(
-            <form>
+            <form onSubmit={this.obtenerDatos}>
                 <div className="row">
+
                     <div className="form-group col-md-8">
-                        <input type="text" className="form-control form-control-lg" 
-                        placeholder="Busca tu imagen. Ejemplo: Futbol"/>
+                        <input ref={this.busquedaRef} type="text" className="form-control form-control-lg" 
+                        placeholder="Realiza tu consulta. Ejemplo: Solicitud pendiente..."/>
                     </div>
                     <div className="form-group col-md-4">
                         <input type="submit" className="btn btn-lg btn-danger btn-block" 
