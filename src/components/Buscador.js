2c567390b3f91cc050d61ead3a4d3d916ca8a73a
pcanjura <pcanjura@gmail.com>
2020-07-12 21:41:12 -0600

    exit

---
+++ src/components/Buscador.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Buscador extends Component {
+    render(){
+        return(
+            <form>
+                <div className="row">
+                    <div className="form-group col-md-8">
+                        <input type="text" className="form-control form-control-lg" 
+                        placeholder="Busca tu imagen. Ejemplo: Futbol"/>
+                    </div>
+                    <div className="form-group col-md-4">
+                        <input type="submit" className="btn btn-lg btn-danger btn-block" 
+                        value="Buscar..."/>
+                    </div>
+
+                </div>
+            </form>
+        );
+
+
+    }
+}
+
+export default Buscador;
